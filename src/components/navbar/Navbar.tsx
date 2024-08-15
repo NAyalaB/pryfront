484fa6269bb3c94b8b7149f69b9c2a19c569ad7f
Noelia <ayala.noe01@gmail.com>
2024-08-15 16:30:24 -0300

    otra prueba

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -30,8 +30,8 @@ const Navbar: React.FC = () => {
             setToken(null);
             setUser(null);
             // window.location.href = `${apiUrl}/auth/logout`;
-            const frontendUrl = process.env.FRONTEND_PROD_URL || process.env.FRONTEND_URL;
-            window.location.href = `${process.env.AUTH0_ISSUER_BASE_URL}/v2/logout?client_id=${process.env.AUTH0_CLIENT_ID}&returnTo=${encodeURIComponent(frontendUrl + '/home')}&federated`;
+            const frontendUrl = process.env.NEXT_PUBLIC_FRONTEND_PROD_URL || process.env.FRONTEND_URL;
+            window.location.href = `${process.env.NEXT_PUBLIC_AUTH0_ISSUER_BASE_URL}/v2/logout?client_id=${process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}&returnTo=${encodeURIComponent(frontendUrl + '/home')}&federated`;
         } catch (error) {
             console.error('Error during logout:', error);
         }
