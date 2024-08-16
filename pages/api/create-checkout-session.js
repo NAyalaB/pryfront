f2c7eb6b2bfaef5ead68074f0d4361dfdc9ae4b2
amandabrousson <amanda.brousson@gmail.com>
2024-08-16 19:32:26 -0300

    otra modificacion url

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -1,7 +1,7 @@
 // pages/api/create-checkout-session.js
 import Stripe from "stripe";
 
-const urlHome = process.env.NEXT_PUBLIC_URL_FRONT
+const urlHome = process.env.NEXT_PUBLIC_FRONTEND_PROD_URL
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2024-06-20',
@@ -10,11 +10,9 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 
 
 export default async function handler(req, res) {
-  console.log(`Received request with method: ${req.method}`); // Log el método recibido
   if (req.method === 'POST') {
     try {
-      const { title, price, description, userId, eventId} = req.body;
-      console.log('Request body:', req.body); // Log el cuerpo de la solicitud
+        const { title, price, description, userId, eventId} = req.body;
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         line_items: [
@@ -23,7 +21,7 @@ export default async function handler(req, res) {
               currency: 'usd',
               product_data: {
                 name: title,
-                description: description,
+                description:description,
               },
               unit_amount: price * 100,
             },
@@ -33,7 +31,7 @@ export default async function handler(req, res) {
         mode: 'payment',
         success_url: `${urlHome}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${urlHome}/experience`,
-        metadata: {
+        metadata:{
           userId,
           eventId
         }
