fea608b8a82349cc84bbbdadd459acef89cfb4e6
amandabrousson <amanda.brousson@gmail.com>
2024-08-16 20:14:23 -0300

    url checkout

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -1,18 +1,23 @@
 // pages/api/create-checkout-session.js
 import Stripe from "stripe";
 
-const urlHome = process.env.NEXT_PUBLIC_FRONTEND_PROD_URL
-
+const frontendUrl = process.env.NEXT_PUBLIC_FRONTEND_PROD_URL || process.env.NEXT_PUBLIC_FRONTEND_URL;   
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2024-06-20',
 });
- 
+
 
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-        const { title, price, description, userId, eventId} = req.body;
+      const { title, price, description, quantity, userId, eventsId } = req.body;
+
+      if (!quantity || quantity < 1) {
+        res.status(400).json({ error: "Invalid quantity" });
+        return;
+      }
+
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         line_items: [
@@ -21,21 +26,27 @@ export default async function handler(req, res) {
               currency: 'usd',
               product_data: {
                 name: title,
-                description:description,
+                description: description,
               },
               unit_amount: price * 100,
             },
-            quantity: 1,
+            quantity: quantity,
           },
         ],
         mode: 'payment',
-        success_url: `${urlHome}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${urlHome}/experience`,
-        metadata:{
-          userId,
-          eventId
+
+
+
+        success_url: `${frontendUrl}/success?session_id={CHECKOUT_SESSION_ID}`,
+
+        cancel_url: `${frontendUrl}/experience`,
+        metadata: {
+          eventId: eventsId,
+          userId: userId
         }
       });
+
+
 
       res.status(200).json({ sessionId: session.id });
     } catch (error) {
