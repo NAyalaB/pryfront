f122f4cb34dda3ea80d7f96ddaa56be0b7e0881f
amandabrousson <amanda.brousson@gmail.com>
2024-08-17 19:08:51 -0300

    manejo responsive eventos user dashboard

--- src/components/dashboard/User/EventDashboard.tsx
+++ src/components/dashboard/User/EventDashboard.tsx
@@ -16,10 +16,10 @@ const EventDashboard: React.FC<EventDashboardProps> = ({ events, bookings }) => 
     <section className="flex flex-col justify-center items-center bg-gray-500-50 rounded-md w-[90%] mx-auto space-y-6 p-5 my-20">
       <div className="bg-white bg-opacity-30 w-full flex flex-col text-center h-auto rounded-md mx-auto space-y-6 p-6">
         <h1 className="text-gray-100 text-3xl font-bold underline">🎟️ Active Events</h1>
-        <ul className="flex flex-wrap gap-4">
+        <ul className="flex flex-wrap gap-4 justify-center">
           {activeEvents.length > 0 ? (
             activeEvents.map(booking => (
-              <li key={booking.TransactionNumber} className="flex-none bg-gray-200 p-4 rounded-md shadow-md w-1/4">
+              <li key={booking.TransactionNumber} className="flex-none bg-gray-200 p-4 rounded-md shadow-md w-full sm:w-2/3 md:w-5/12">
                 <h3 className="text-gray-800 text-md font-semibold">{booking.eventTitle}</h3>
                 <p className="text-gray-600 text-sm">Transaction: {booking.TransactionNumber}</p>
                 <p className="text-gray-600 text-sm">Quantity: {booking.Quantity}</p>
@@ -40,10 +40,10 @@ const EventDashboard: React.FC<EventDashboardProps> = ({ events, bookings }) => 
 
       <div className="bg-white bg-opacity-30 w-full flex flex-col text-center h-auto rounded-md mx-auto space-y-6 p-6">
         <h1 className="text-gray-100 text-3xl font-bold underline">📖 Event History</h1>
-        <ul className="flex flex-wrap gap-4">
+        <ul className="flex flex-wrap gap-4 justify-center">
           {eventHistory.length > 0 ? (
             eventHistory.map(booking => (
-              <li key={booking.TransactionNumber} className="flex-none bg-gray-200 p-4 rounded-md shadow-md w-1/4">
+              <li key={booking.TransactionNumber} className="flex-none bg-gray-200 p-4 rounded-md shadow-md w-full sm:w-2/3 md:w-5/12">
                 <h3 className="text-gray-800 text-md font-semibold">Event: {booking.eventTitle}</h3>
                 <p className="text-gray-600 text-sm">Transaction: {booking.TransactionNumber}</p>
                 <p className="text-gray-600 text-sm">Quantity: {booking.Quantity}</p>
